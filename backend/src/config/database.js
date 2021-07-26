5702a59aacb39a9ff6bbd4da1d24764cea169665
LuisLimaIt <pls.luis@gmail.com>
2021-07-26 13:26:47 -0300

    Testes e ajustes na API

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,3 +2,10 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 module.exports = mongoose.connect('mongodb://localhost/mymoney');
 
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório.";
+mongoose.Error.messages.Number.min = 
+    "'{VALUE}' é menor que o limite mínimo de '{MIN}'.";
+mongoose.Error.messages.Number.max = 
+    "'{VALUE}' é maior que o limite máximo' de '{MAX}'.";
+mongoose.Error.messages.String.enum =
+    "{VALUE} não é válido para o atributo '{PATH}'."
