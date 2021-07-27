05a95b7278e2b483df0160a0a37c675017d0855f
LuisLimaIt <pls.luis@gmail.com>
2021-07-27 11:35:54 -0300

    Obtendo a Qtde de Registros (Count)

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -13,4 +13,14 @@ BillingCycle.route('get', (req, res, next) => {
     });
 });
 
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error) {
+            res.status(500).json({erros: [error]});
+        } else {
+            res.json({value});
+        }
+    })
+});
+
 module.exports = BillingCycle;
