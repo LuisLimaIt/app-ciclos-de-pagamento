a24630ead2bf6c0368c71c717daa1ebcfdf1d075
LuisLimaIt <pls.luis@gmail.com>
2021-07-30 13:59:34 -0300

    Componente Rodapé

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,10 +3,15 @@ import React from 'react';
 
 import Header from '../common/template/header';
 import SideBar from '../common/template/sidebar';
+import Footer from '../common/template/footer';
 
 export default props => (
     <div className='wrapper'>
         <Header />
         <SideBar />
+        <div className="content-wrapper">
+            <h1>Conteúdo</h1>
+        </div>
+        <Footer />
     </div>
 );
