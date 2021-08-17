dbecff2594b0bd658fe09d8eab1c0de6109b588b
LuisLimaIt <pls.luis@gmail.com>
2021-08-16 23:10:54 -0300

    Inicializar Formulário com Dados

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { toastr } from 'react-redux-toastr';
-import { reset as resetForm } from 'redux-form';
+import { reset as resetForm, initialize } from 'redux-form';
 import { showTabs, selectTab } from '../common/tab/tabActions';
 
 const BASE_URL = 'http://localhost:3001/api';
@@ -35,5 +35,6 @@ export function showUpdate(billingCycle) {
     return [
         showTabs('tabUpdate'),
         selectTab('tabUpdate'),
+        initialize('billingCycleForm', billingCycle)
     ]
 };
