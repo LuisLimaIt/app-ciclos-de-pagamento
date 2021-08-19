3e413c484b4c45dfa2071a053b756e3d1532dceb
LuisLimaIt <pls.luis@gmail.com>
2021-08-19 12:48:42 -0300

    Componente Input para CreditList

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux';
 import { reduxForm, Field } from 'redux-form';
 
 import { init } from './billingCycleActions';
-import labelAndInput from '../common/form/labelAndInput';
+import LabelAndInput from '../common/form/labelAndInput';
 import CreditList from './creditList';
 
 class BillingCycleForm extends Component {
@@ -14,16 +14,16 @@ class BillingCycleForm extends Component {
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                    <Field name='name' component={labelAndInput} readOnly={readOnly}
+                    <Field name='name' component={LabelAndInput} readOnly={readOnly}
                         label='Nome' cols='12 4' placeholder='Informe o nome'    
                     />
-                    <Field name='month' component={labelAndInput} type='number' readOnly={readOnly}
+                    <Field name='month' component={LabelAndInput} type='number' readOnly={readOnly}
                         label='Mês' cols='12 4' placeholder='Informe o mês'    
                     />
-                    <Field name='year' component={labelAndInput} type='number' readOnly={readOnly}
+                    <Field name='year' component={LabelAndInput} type='number' readOnly={readOnly}
                         label='Ano' cols='12 4' placeholder='Informe o ano'    
                     />
-                    <CreditList cols='12 6' />
+                    <CreditList cols='12 6' readOnly={readOnly} />
                 </div>
                 <div className='box-footer'>
                     <button type='submit 'className={`btn btn-${this.props.submitClass}`}>
--- frontend/src/billingCycle/creditList.jsx
+++ frontend/src/billingCycle/creditList.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { Field } from 'redux-form';
 import Grid from '../common/layout/grid';
+import Input from '../common/form/input';
 
 class CreditList extends Component {
 
     renderRows() {
         return (
             <tr>
-                <td><Field name='credits[0].name' component='input' /></td>
-                <td><Field name='credits[0].value' component='input' /></td>
+                <td><Field name='credits[0].name' component={Input} 
+                placeholder="Informe o nome" readOnly={this.props.readOnly} /></td>
+                <td><Field name='credits[0].value' component={Input} 
+                placeholder="Informe o valor" readOnly={this.props.readOnly} /></td>
                 <td></td>
             </tr>
         )
