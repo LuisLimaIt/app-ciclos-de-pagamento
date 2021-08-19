a5cd3b13a9b73a44c48d914ba181db56fc15bf27
LuisLimaIt <pls.luis@gmail.com>
2021-08-19 12:22:39 -0300

    Componente CreditList Básico

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,6 +5,7 @@ import { reduxForm, Field } from 'redux-form';
 
 import { init } from './billingCycleActions';
 import labelAndInput from '../common/form/labelAndInput';
+import CreditList from './creditList';
 
 class BillingCycleForm extends Component {
 
@@ -22,6 +23,7 @@ class BillingCycleForm extends Component {
                     <Field name='year' component={labelAndInput} type='number' readOnly={readOnly}
                         label='Ano' cols='12 4' placeholder='Informe o ano'    
                     />
+                    <CreditList cols='12 6' />
                 </div>
                 <div className='box-footer'>
                     <button type='submit 'className={`btn btn-${this.props.submitClass}`}>
