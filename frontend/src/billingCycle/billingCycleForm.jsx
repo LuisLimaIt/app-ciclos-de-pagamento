16267139f51abbd7155a8b850fc1b790b11e086e
LuisLimaIt <pls.luis@gmail.com>
2021-08-20 14:31:49 -0300

    Componente Summary

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -6,6 +6,7 @@ import { reduxForm, Field, formValueSelector } from 'redux-form';
 import { init } from './billingCycleActions';
 import LabelAndInput from '../common/form/labelAndInput';
 import ItemList from './itemList';
+import Summary from './summary';
 
 class BillingCycleForm extends Component {
 
@@ -23,6 +24,7 @@ class BillingCycleForm extends Component {
                     <Field name='year' component={LabelAndInput} type='number' readOnly={readOnly}
                         label='Ano' cols='12 4' placeholder='Informe o ano'    
                     />
+                    <Summary credit={1000} debt={100} />
                     <ItemList cols='12 6' list={credits} readOnly={readOnly} 
                         field='credits' legend='Créditos' />
 
