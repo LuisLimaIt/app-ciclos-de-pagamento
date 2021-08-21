883b814295cdfc6a42f501263eee0a38db12d0d5
LuisLimaIt <pls.luis@gmail.com>
2021-08-20 23:58:08 -0300

    Integrar Soma de Créditos e Débitos

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -10,8 +10,17 @@ import Summary from './summary';
 
 class BillingCycleForm extends Component {
 
+    calculateSummary() {
+        const sum = (t, v) => t + v;
+        return {
+            sumOfCredits: this.props.credits.map(c => +c.value || 0).reduce(sum),
+            sumOfDebts: this.props.debts.map(d => +d.value || 0).reduce(sum)
+        }
+    }
+
     render() {
         const { handleSubmit, readOnly, credits, debts } = this.props;
+        const { sumOfCredits, sumOfDebts } = this.calculateSummary();
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
@@ -24,7 +33,7 @@ class BillingCycleForm extends Component {
                     <Field name='year' component={LabelAndInput} type='number' readOnly={readOnly}
                         label='Ano' cols='12 4' placeholder='Informe o ano'    
                     />
-                    <Summary credit={1000} debt={100} />
+                    <Summary credit={sumOfCredits} debt={sumOfDebts} />
                     <ItemList cols='12 6' list={credits} readOnly={readOnly} 
                         field='credits' legend='Créditos' />
 
