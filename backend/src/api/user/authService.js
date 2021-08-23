4d551198f128b4597d2f43ded1fe9f085f883d0e
LuisLimaIt <pls.luis@gmail.com>
2021-08-23 11:47:43 -0300

    Autenticação - Backend (Parte 06)

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -39,3 +39,47 @@ const validateToken = (req, res, next) => {
         return res.status(200).send({ valid: !err });
     });
 };
+
+const signup = (req, res, next) => {
+    const name = req.body.name || ''
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+    const confirmPassword = req.body.confirm_password || ''
+
+    if(!email.match(emailRegex)) {
+        return res.status(400).send({errors: ['O e-mail informado é inválido']})
+    }
+
+    if(!password.match(passwordRegex)) { 
+        return res.status(400).send({
+            errors: [
+                "Senha precisar ter: uma letra maiúscula, uma letra minúscula, um número, uma caractere especial(@#$%) e tamanho entre 6-20."
+            ]
+        }); 
+    };
+
+    const salt = bcrypt.genSaltSync();
+    const passwordHash = bcrypt.hashSync(password, salt);
+    if(!bcrypt.compareSync(confirmPassword, passwordHash)) {
+        return res.status(400).send({errors: ['Senhas não conferem.'] }); 
+    };
+
+    User.findOne({email}, (err, user) => { 
+        if(err) {
+            return sendErrorsFromDB(res, err); 
+        } else if (user) {
+            return res.status(400).send({errors: ['Usuário já cadastrado.']}); 
+        } else {
+            const newUser = new User({ name, email, password: passwordHash }); 
+            newUser.save(err => {
+                if(err) {
+                    return sendErrorsFromDB(res, err)
+                } else {
+                    login(req, res, next)
+                }; 
+            });
+        }; 
+    });        
+};
+
+module.exports = { login, signup, validateToken };
