5086b2d1f3b9ae44681e692ad538d2197af02e43
LuisLimaIt <pls.luis@gmail.com>
2021-08-24 12:46:43 -0300

    Autenticação - Frontend (Parte 02)

--- frontend/src/auth/authReducer.js
+++ frontend/src/auth/authReducer.js
@@ -1,7 +1,7 @@
 const userKey = '_mymoney_user'; 
 const INITIAL_STATE = {
     user: JSON.parse(localStorage.getItem(userKey)),
-validToken: false 
+    validToken: false 
 };
 
 export default (state = INITIAL_STATE, action) => { 
