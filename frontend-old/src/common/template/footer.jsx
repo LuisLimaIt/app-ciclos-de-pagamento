f966805273e48e764c49e5a1bd4688c9f01b9202
LuisLimaIt <pls.luis@gmail.com>
2021-08-25 13:44:55 -0300

    Atualizando as bibliotecas

---
+++ frontend-old/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; Luis Paulo de Lima Soares 2021
+            <a href='https://github.com/LuisLimaIt' target='_blank'> GitHub</a>. 
+        </strong>
+    </footer>
+)
